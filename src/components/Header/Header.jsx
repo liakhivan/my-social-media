dbe41a24bcb0ee5f5541a5ed6504417d17655911
Ivan Liakh <liakhivan21@gmail.com>
2022-01-27 22:28:31 +0200

    Fix formating

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,13 @@ import './Header.scss';
 function Header() {
     return (
         <header className='header'>
-        <a className='logo' href='s#'>
-            <img 
-            alt='logo' 
-            src='https://i.pinimg.com/favicons/caf24485156b34efc78475d1e701073dcd57f87b7926e38c906fe148.ico?2bafee7fc8765a4d4839557599723174' 
-            />
-            <p>MSM</p>
-        </a>
+            <a className='logo' href='s#'>
+                <img 
+                alt='logo' 
+                src='https://i.pinimg.com/favicons/caf24485156b34efc78475d1e701073dcd57f87b7926e38c906fe148.ico?2bafee7fc8765a4d4839557599723174' 
+                />
+                <p>MSM</p>
+            </a>
         </header>
     );
 }
